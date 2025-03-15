fca740ead9cb248e1d5b787fac673851d64b918e
Phil Dubach <phil.dubach@thingstream.io>
2025-03-14 18:50:11 -0700

    Layout fix and add LLM options

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 import streamlit as st
 import ollama
 
@@ -22,22 +24,36 @@ if 'selected_model' not in st.session_state:
     else:
         st.session_state.selected_model = None
 
-col1, col2 = st.columns(2)
+model_index = find_model_index(st.session_state.selected_model)
 
-index = find_model_index(st.session_state.selected_model)
+st.sidebar.button(label='Clear chat', key='clear_chat',
+                  use_container_width=True, on_click=lambda:
+                  st.session_state.chat_history.clear())
+
+col1, col2 = st.sidebar.columns([0.8, 0.2], vertical_alignment='bottom')
 
 with col1:
-    selected_model = st.sidebar.selectbox(
+    selected_model = st.selectbox(
         "Select the model to use",
         st.session_state.available_models,
-        index=index,
+        index=model_index,
         format_func=lambda model: model.model
     )
     st.session_state.selected_model = selected_model
 
 with col2:
-    if st.sidebar.button(icon=":material/autorenew:", label=''):
+    if st.button(icon=":material/autorenew:", label=''):
         load_models()
+
+temp = st.sidebar.number_input(label='Temperature (0.8)',min_value=0.0,
+                               max_value=2.0, step=0.05, value=None,
+                               key='temperature')
+seed = st.sidebar.number_input(label='Seed (0)',min_value=0, max_value=2**32-1,
+                               value=None)
+num_ctx = st.sidebar.number_input(label='Context length (2048)',min_value=2048,
+                                  value=None)
+num_predict = st.sidebar.number_input(label='Output length (-1)', min_value=-1,
+                                      value=None)
 
 if st.session_state.selected_model:
     st.sidebar.markdown("### Model details")
@@ -50,10 +66,9 @@ if st.session_state.selected_model:
             ddict['Quant'] = details['quantization_level']
         if 'family' in details:
             ddict['Family'] = details['family']
-        st.sidebar.write(ddict)
+        st.sidebar.table(ddict)
 
 st.title("Ollama Chat Frontend")
-st.caption("This is a simple chat frontend for the Ollama chatbot.")
 
 # Initialize chat history in session state
 if "chat_history" not in st.session_state:
@@ -63,11 +78,23 @@ for message in st.session_state.chat_history:
     st.chat_message(message["role"]).write(message["content"])
 
 def get_ollama_response():
+    options : ollama.Options = {}
+    if temp is not None:
+        options['temperature'] = temp
+    if seed is not None:
+        options['seed'] = seed
+    else:
+        options['seed'] = random.randint(0, 2**32-1)
+    if num_ctx is not None:
+        options['num_ctx'] = num_ctx
+    if num_predict is not None:
+        options['num_predict'] = num_predict
     full_response = ''
     response = ollama.chat(
         model=st.session_state.selected_model.model,
         messages=st.session_state.chat_history,
-        stream=True
+        stream=True,
+        options=options,
     )
     for chunk in response:
         text = chunk['message']['content']
@@ -83,3 +110,5 @@ if prompt := st.chat_input():
     add_message('user', prompt)
     st.chat_message('user').write(prompt)
     st.chat_message('assistant').write(get_ollama_response())
+
+
